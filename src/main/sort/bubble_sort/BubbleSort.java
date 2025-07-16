e0ba3d8f3f8d3d990341b1f0a3d7157fc6fefe55
Taher <taherc.ali@gmail.com>
2025-07-16 21:34:41 +0530

    bubble sort

--- src/main/sort/bubble_sort/BubbleSort.java
+++ src/main/sort/bubble_sort/BubbleSort.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 
 public class BubbleSort {
 
-	public static void bubbleSort(int nums[]) {
-		for (int i = nums.length - 1; i >= 0; i--) {
+	public static void bubbleSort(int[] nums) {
+		for (int i = nums.length - 1; i > 0; i--) {
 			for (int j = 0; j < i; j++) {
 				if (nums[j] > nums[j + 1]) {
 					int temp = nums[j];
@@ -17,10 +17,10 @@ public class BubbleSort {
 	}
 
 	public static void main(String[] args) {
-		int nums[] = { 4, 2, 6, 5, 1, 3 };
-		System.out.println("before sorting : " + Arrays.toString(nums));
+		int[] nums = { 4, 2, 6, 5, 1, 3 };
+		System.out.println("before  sorting " + Arrays.toString(nums));
 		bubbleSort(nums);
-		System.out.println("after sorting  : " + Arrays.toString(nums));
+		System.out.println("after   sorting " + Arrays.toString(nums));
 	}
 
 }
