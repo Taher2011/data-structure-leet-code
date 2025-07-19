e70a22c5a284d90e5e1db8032b6b5202332c0809
Taher <taherc.ali@gmail.com>
2025-07-19 09:18:24 +0530

    remove duplicates

--- src/main/array-interview/array_remove_duplicates/RemoveDuplicates.java
+++ src/main/array-interview/array_remove_duplicates/RemoveDuplicates.java
@@ -4,60 +4,83 @@ import java.util.Arrays;
 
 public class RemoveDuplicates {
 
-	public static int removeDuplicates(int[] nums) {
-		if (nums.length == 0) {
-			return 0;
-		}
-		int writer = 1;
-		for (int reader = 1; reader < nums.length; reader++) {
-			if (nums[reader] != nums[reader - 1]) {
-				nums[writer] = nums[reader];
-				writer++;
+	public static int removeDuplicatesFromSortedArray(int[] nums) {
+		if (nums.length != 0) {
+			int writer = 1;
+			for (int reader = 1; reader < nums.length; reader++) {
+				if (nums[reader] != nums[reader - 1]) {
+					nums[writer] = nums[reader];
+					writer++;
+				}
 			}
+			return writer;
 		}
-		return writer;
+		return 0;
 	}
 
 	public static <T> int removeDuplicatesFromUnsortedArray(T[] nums) {
-		if (nums.length == 0) {
-			return 0;
-		}
-		int writer = 1;
-		for (int reader = 1; reader < nums.length; reader++) {
-			boolean isDuplicate = false;
-			for (int i = 0; i < writer; i++) {
-				if (nums[reader].equals(nums[i])) {
-					isDuplicate = true;
-					break;
+		if (nums.length != 0) {
+			int writer = 1;
+			for (int reader = 1; reader < nums.length; reader++) {
+				boolean isDuplicate = false;
+				for (int i = 0; i < reader; i++) {
+					if (nums[i] == nums[reader]) {
+						isDuplicate = true;
+						break;
+					}
+				}
+				if (!isDuplicate) {
+					nums[writer] = nums[reader];
+					writer++;
 				}
 			}
-			if (!isDuplicate) {
-				nums[writer] = nums[reader];
-				writer++;
-			}
+			return writer;
 		}
-		return writer;
+		return 0;
 	}
 
 	public static void main(String[] args) {
-		int[] nums1 = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
-		System.out.println(Arrays.toString(nums1));
-		int length = removeDuplicates(nums1);
-		System.out.println("list after duplicates removal : " + Arrays.toString(Arrays.copyOfRange(nums1, 0, length)));
+		Integer[] nums1 = { 5, 1, 4, 7, 5, 1, 4, 0, 2, 2, 7, 5, 4 };
+		System.out.println(Arrays.toString(Arrays.copyOfRange(nums1, 0, nums1.length)));
+		int newLength1 = removeDuplicatesFromUnsortedArray(nums1);
+		System.out.println(Arrays.toString(Arrays.copyOfRange(nums1, 0, newLength1)));
+		System.out.println("=====================================================");
 
-		System.out.println();
+		int[] nums2 = { 1, 1, 2, 2, 2, 3 };
+		System.out.println(Arrays.toString(Arrays.copyOfRange(nums2, 0, nums2.length)));
+		int newLength2 = removeDuplicatesFromSortedArray(nums2);
+		System.out.println(Arrays.toString(Arrays.copyOfRange(nums2, 0, newLength2)));
+		System.out.println("=====================================================");
 
-		Integer[] nums2 = { 1, 0, 1, 2, 1, 2, 2, 1, 3, 2 };
-		System.out.println(Arrays.toString(nums2));
-		length = removeDuplicatesFromUnsortedArray(nums2);
-		System.out.println("list after duplicates removal : " + Arrays.toString(Arrays.copyOfRange(nums2, 0, length)));
+		int[] nums3 = { -1, 0, 0, 0, 3, 3 };
+		System.out.println(Arrays.toString(Arrays.copyOfRange(nums3, 0, nums3.length)));
+		int newLength3 = removeDuplicatesFromSortedArray(nums3);
+		System.out.println(Arrays.toString(Arrays.copyOfRange(nums3, 0, newLength3)));
+		System.out.println("=====================================================");
 
-		System.out.println();
+		int[] nums4 = {};
+		System.out.println(Arrays.toString(Arrays.copyOfRange(nums4, 0, nums4.length)));
+		int newLength4 = removeDuplicatesFromSortedArray(nums4);
+		System.out.println(Arrays.toString(Arrays.copyOfRange(nums4, 0, newLength4)));
+		System.out.println("=====================================================");
 
-		String[] nums3 = { "a", "b", "b", "a", "c", "b" };
-		System.out.println(Arrays.toString(nums3));
-		length = removeDuplicatesFromUnsortedArray(nums3);
-		System.out.println("list after duplicates removal : " + Arrays.toString(Arrays.copyOfRange(nums3, 0, length)));
+		int[] nums5 = { 1, 1, 1, 1, 1 };
+		System.out.println(Arrays.toString(Arrays.copyOfRange(nums5, 0, nums5.length)));
+		int newLength5 = removeDuplicatesFromSortedArray(nums5);
+		System.out.println(Arrays.toString(Arrays.copyOfRange(nums5, 0, newLength5)));
+		System.out.println("=====================================================");
+
+		Integer[] nums6 = { 1, 0, 1, 2, 1, 2, 2, 1, 3, 2 };
+		System.out.println(Arrays.toString(nums6));
+		int length = removeDuplicatesFromUnsortedArray(nums6);
+		System.out.println(Arrays.toString(Arrays.copyOfRange(nums6, 0, length)));
+
+		System.out.println("=====================================================");
+
+		String[] nums7 = { "a", "b", "b", "a", "c", "b" };
+		System.out.println(Arrays.toString(nums7));
+		int length1 = removeDuplicatesFromUnsortedArray(nums7);
+		System.out.println(Arrays.toString(Arrays.copyOfRange(nums7, 0, length1)));
 	}
 
 }
