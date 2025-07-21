2f254ed66cd55119062aaabe0fe4a5d718d75944
Taher <taherc.ali@gmail.com>
2025-07-21 19:08:40 +0530

    find first non repeating character

--- src/main/array-interview-1/first_non_repeating_char/FindFirstNonRepChar.java
+++ src/main/array-interview-1/first_non_repeating_char/FindFirstNonRepChar.java
@@ -2,48 +2,47 @@ package first_non_repeating_char;
 
 public class FindFirstNonRepChar {
 
-	public static String removeDuplicates(String str) {
-		char[] ch = str.toLowerCase().toCharArray();
-		int writer = 1;
-		for (int reader = 0; reader < ch.length; reader++) {
-			boolean isDuplicate = false;
-			for (int i = 0; i < writer; i++) {
-				if (ch[i] == ch[reader]) {
-					isDuplicate = true;
-					break;
+	public static String findDuplicates(String str) {
+		char[] ch = str.toCharArray();
+		StringBuilder builder = new StringBuilder();
+		boolean[] read = new boolean[256];
+		boolean[] write = new boolean[256];
+		for (int i = 0; i < ch.length; i++) {
+			if (!read[ch[i]]) {
+				read[ch[i]] = true;
+			} else {
+				if (!write[ch[i]]) {
+					builder.append(ch[i]);
+					write[ch[i]] = true;
 				}
 			}
-			if (!isDuplicate) {
-				ch[writer] = ch[reader];
-				writer++;
-			}
 		}
-		return new String(ch, 0, writer);
+		return new String(builder);
 	}
 
 	public static String findFirstNonRepChar(String str) {
-		String unqiueStr = removeDuplicates(str);
-		char[] ch1 = unqiueStr.toCharArray();
-		char[] ch2 = str.toCharArray();
+		String duplicates = findDuplicates(str);
+		char[] ch1 = str.toCharArray();
+		char[] ch2 = duplicates.toCharArray();
 		for (int i = 0; i < ch1.length; i++) {
-			int count = 0;
+			int counter = 0;
 			for (int j = 0; j < ch2.length; j++) {
 				if (ch1[i] == ch2[j]) {
-					count++;
+					counter++;
+					break;
 				}
 			}
-			if (count < 2) {
-				return "'" + String.valueOf(ch1[i]) + "' is the first non repeating char in the " + str;
+			if (counter < 1) {
+				return new String(ch1[i] + "");
 			}
 		}
-
-		return "no non repeating char are present";
+		return duplicates;
 	}
 
 	public static void main(String[] args) {
 		String str = "malayalam";
 		String firstNonRepChar = findFirstNonRepChar(str);
-		System.out.println(firstNonRepChar);
+		System.out.println("'" + firstNonRepChar + "' is the first non repeating char in the " + str);
 	}
 
 }
