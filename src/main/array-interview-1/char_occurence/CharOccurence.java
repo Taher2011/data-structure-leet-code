08e17b1469ce5898dca5b521f0ddeb421f9b2862
Taher <taherc.ali@gmail.com>
2025-07-21 14:44:38 +0530

    character occurrences

--- src/main/array-interview-1/char_occurence/CharOccurence.java
+++ src/main/array-interview-1/char_occurence/CharOccurence.java
@@ -4,22 +4,52 @@ import java.util.Arrays;
 
 public class CharOccurence {
 
-	public static void charOccurence(String str) {
-		if (str.isEmpty()) {
-			return;
-		}
-		String unqiueStr = removeDuplicates(str);
-		char[] ch1 = unqiueStr.toCharArray();
-		char[] ch2 = str.toCharArray();
-		for (int i = 0; i < ch1.length; i++) {
-			int count = 0;
-			for (int j = 0; j < ch2.length; j++) {
-				if (ch1[i] == ch2[j]) {
-					count++;
+	public static String removeDupilcates(String str) {
+		char[] ch = str.toCharArray();
+		int writer = 1;
+		for (int reader = 1; reader < ch.length; reader++) {
+			boolean isDuplicate = false;
+			for (int i = 0; i < reader; i++) {
+				if (ch[i] == ch[reader]) {
+					isDuplicate = true;
+					break;
 				}
 			}
-			System.out.println("char '" + ch1[i] + "' occur " + count + " times");
+			if (!isDuplicate) {
+				ch[writer] = ch[reader];
+				writer++;
+			}
 		}
+		return new String(Arrays.copyOfRange(ch, 0, writer));
+	}
+
+	public static void characterOccurrences(String str) {
+		String unique = removeDupilcates1(str);
+		char[] ch1 = unique.toCharArray();
+		char[] ch2 = str.toCharArray();
+		for (int i = 0; i < ch1.length; i++) {
+			int counter = 0;
+			for (int j = 0; j < ch2.length; j++) {
+				if (ch1[i] == ch2[j]) {
+					counter++;
+				}
+			}
+			System.out.println("char " + ch1[i] + " occured " + counter + " time");
+		}
+	}
+
+	public static String removeDupilcates1(String str) {
+		char[] ch = str.toCharArray();
+		StringBuilder sb = new StringBuilder();
+		boolean[] seen = new boolean[256];
+		boolean[] read = new boolean[256];
+		for (int i = 0; i < ch.length; i++) {
+			if (!seen[ch[i]]) {
+				seen[ch[i]] = true;
+				sb.append(ch[i]);
+			}
+		}
+		return new String(sb);
 	}
 
 	public static void stringOccurence(String string) {
@@ -51,28 +81,9 @@ public class CharOccurence {
 		}
 	}
 
-	public static String removeDuplicates(String str) {
-		char[] ch = str.toLowerCase().toCharArray();
-		int writer = 1;
-		for (int reader = 0; reader < ch.length; reader++) {
-			boolean isDuplicate = false;
-			for (int i = 0; i < writer; i++) {
-				if (ch[i] == ch[reader]) {
-					isDuplicate = true;
-					break;
-				}
-			}
-			if (!isDuplicate) {
-				ch[writer] = ch[reader];
-				writer++;
-			}
-		}
-		return new String(ch, 0, writer);
-	}
-
 	public static void main(String[] args) {
-		charOccurence("a");
-		System.out.println();
+		characterOccurrences("malayalam");
+		System.out.println("=============================");
 		String string = "this is chair and this is table";
 		stringOccurence(string);
 	}
