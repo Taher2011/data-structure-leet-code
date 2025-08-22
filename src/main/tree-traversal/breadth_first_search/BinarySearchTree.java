d65055e03470f9c85c6f6cff55338e4def99fef2
Taher <taherc.ali@gmail.com>
2025-08-22 18:31:46 +0530

    tree traversal using BFS

--- src/main/tree-traversal/breadth_first_search/BinarySearchTree.java
+++ src/main/tree-traversal/breadth_first_search/BinarySearchTree.java
@@ -66,12 +66,12 @@ public class BinarySearchTree {
 
 	public static void main(String[] args) {
 		BinarySearchTree bst = new BinarySearchTree();
+		bst.rInsert(47);
 		bst.rInsert(21);
 		bst.rInsert(76);
 		bst.rInsert(18);
 		bst.rInsert(27);
 		bst.rInsert(52);
-		bst.rInsert(47);
 		bst.rInsert(82);
 		System.out.println(bst.BFS());
 	}
