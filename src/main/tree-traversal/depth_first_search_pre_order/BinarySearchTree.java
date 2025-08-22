9cff6b0603ff9b55a0311e01d74c9d92f8c28616
Taher <taherc.ali@gmail.com>
2025-08-22 15:29:02 +0530

    tree traversal using DFS pre order

--- src/main/tree-traversal/depth_first_search_pre_order/BinarySearchTree.java
+++ src/main/tree-traversal/depth_first_search_pre_order/BinarySearchTree.java
@@ -1,6 +1,7 @@
 package depth_first_search_pre_order;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class BinarySearchTree {
 
@@ -44,7 +45,7 @@ public class BinarySearchTree {
 		return rInsert(root, value);
 	}
 
-	public ArrayList<Integer> traverse(Node node, ArrayList<Integer> results) {
+	public List<Integer> traverse(Node node, List<Integer> results) {
 		results.add(node.value);
 		if (node.left != null) {
 			traverse(node.left, results);
@@ -55,21 +56,21 @@ public class BinarySearchTree {
 		return results;
 	}
 
-	public ArrayList<Integer> DFSPreOrder() {
-		ArrayList<Integer> results = new ArrayList<>();
-		traverse(root, results);
-//		class Traverse {
-//			private Traverse(Node node) {
-//				results.add(node.value);
-//				if (node.left != null) {
-//					new Traverse(node.left);
-//				}
-//				if (node.right != null) {
-//					new Traverse(node.right);
-//				}
-//			}
-//		}
-//		new Traverse(root);
+	public List<Integer> DFSPreOrder() {
+		List<Integer> results = new ArrayList<>();
+		class Traverse {
+			public Traverse(Node node) {
+				results.add(node.value);
+				if (node.left != null) {
+					new Traverse(node.left);
+				}
+				if (node.right != null) {
+					new Traverse(node.right);
+				}
+			}
+		}
+		new Traverse(root);
+		// traverse(root, results);
 		return results;
 	}
 
