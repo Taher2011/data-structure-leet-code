be1183a4a21ff510d38e9831fd05598db24efdf3
Taher <taherc.ali@gmail.com>
2025-08-24 08:58:16 +0530

    merge sort

--- src/main/sort/merge_sort/MergeSort.java
+++ src/main/sort/merge_sort/MergeSort.java
@@ -40,13 +40,13 @@ public class MergeSort {
 	}
 
 	public static void main(String[] args) {
-		int a1[] = { 2, 4, 6, 8 };
-		int a2[] = { 3, 5, 7, 9 };
-		int a3[] = merge(a1, a2);
+		int[] a1 = { 2, 4, 6, 8 };
+		int[] a2 = { 3, 5, 7, 9 };
+		int[] a3 = merge(a1, a2);
 		System.out.println(Arrays.toString(a3));
 		System.out.println("========================");
 
-		int a4[] = { 3, 5, 7, 9, 2, 4, 6, 8 };
+		int[] a4 = { 3, 5, 7, 9, 2, 4, 6, 8 };
 		System.out.println("before  sorting " + Arrays.toString(a4));
 		a4 = mergeSort(a4);
 		System.out.println("after   sorting " + Arrays.toString(a4));
