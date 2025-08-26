6f3e4f7442d95ffbe3fc9e17bd26467c9c62ea12
Taher <taherc.ali@gmail.com>
2025-08-26 12:36:25 +0530

    find max sub array

--- src/main/array-interview/array_max_sub_array/MaxSubArray.java
+++ src/main/array-interview/array_max_sub_array/MaxSubArray.java
@@ -4,13 +4,20 @@ import java.util.Arrays;
 
 public class MaxSubArray {
 
-	public static int maxSubarray(int[] nums) {
+	public static int maxSubarray1(int[] nums) {
 		if (nums.length != 0) {
-			int maxSum = nums[0];
 			int currentSum = nums[0];
+			int maxSum = nums[0];
 			for (int i = 1; i < nums.length; i++) {
-				currentSum = Math.max(nums[i], nums[i] + currentSum);
-				maxSum = Math.max(maxSum, currentSum);
+				int totalSubArraySum = nums[i] + currentSum;
+				if (nums[i] > totalSubArraySum) {
+					currentSum = nums[i];
+				} else {
+					currentSum = totalSubArraySum;
+				}
+				if (currentSum > maxSum) {
+					maxSum = currentSum;
+				}
 			}
 			return maxSum;
 		}
@@ -18,29 +25,24 @@ public class MaxSubArray {
 	}
 
 	// alternate approach
-//	public static int maxSubarray(int[] nums) {
-//		if (nums.length != 0) {
-//			int maxSum = nums[0];
-//			int currentSum = nums[0];
-//			for (int i = 1; i < nums.length; i++) {
-//				int totalOfCurrentAndPrevious = nums[i] + currentSum;
-//				if (nums[i] > totalOfCurrentAndPrevious) {
-//					currentSum = nums[i];
-//				} else {
-//					currentSum = totalOfCurrentAndPrevious;
-//				}
-//				if (currentSum > maxSum) {
-//					maxSum = currentSum;
-//				}
-//			}
-//			return maxSum;
-//		}
-//		return 0;
-//	}
+	public static int maxSubarray(int[] nums) {
+		if (nums.length != 0) {
+			int currentSum = nums[0];
+			int maxSum = nums[0];
+			for (int i = 1; i < nums.length; i++) {
+				int totalSubArraySum = nums[i] + currentSum;
+				currentSum = Math.max(nums[i], totalSubArraySum);
+				maxSum = Math.max(maxSum, currentSum);
+			}
+			return maxSum;
+		}
+		return 0;
+	}
 
 	public static void main(String[] args) {
 		// Example 1: Simple case with positive and negative numbers
 		int[] inputCase1 = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+		System.out.println("Example 1: Input: " + Arrays.toString(inputCase1));
 		int result1 = maxSubarray(inputCase1);
 		System.out.println("Expected output : 6");
 		System.out.println("Example 1: Input: " + Arrays.toString(inputCase1) + "\nActual   result : " + result1);
@@ -48,6 +50,7 @@ public class MaxSubArray {
 
 		// Example 2: Case with a negative number in the middle
 		int[] inputCase2 = { 1, 2, 3, -4, 5, 6 };
+		System.out.println("Example 2: Input: " + Arrays.toString(inputCase2));
 		int result2 = maxSubarray(inputCase2);
 		System.out.println("Expected output : 13");
 		System.out.println("Example 2: Input: " + Arrays.toString(inputCase2) + "\nActual   result : " + result2);
