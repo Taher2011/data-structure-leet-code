116da4f54a941f343c560998a500828ad25c5563
Taher <taherc.ali@gmail.com>
2025-08-27 10:09:39 +0530

    anagram

--- src/main/array-interview-1/anagram/Anagram.java
+++ src/main/array-interview-1/anagram/Anagram.java
@@ -2,37 +2,35 @@ package anagram;
 
 public class Anagram {
 
-	public static void sort(char[] a) {
-		for (int i = a.length - 1; i >= 0; i--) {
+	public static void sort(char[] ch) {
+		for (int i = ch.length - 1; i > 0; i--) {
 			for (int j = 0; j < i; j++) {
-				if (a[j] > a[j + 1]) {
-					char temp = a[j];
-					a[j] = a[j + 1];
-					a[j + 1] = temp;
+				if (ch[j] > ch[j + 1]) {
+					char temp = ch[j];
+					ch[j] = ch[j + 1];
+					ch[j + 1] = temp;
 				}
 			}
 		}
 	}
 
 	public static boolean isAnagram(String s1, String s2) {
-		if (s1 == null || s2 == null) {
-			return false;
-		}
-		if (s1.length() != s2.length()) {
-			return false;
-		}
-		char[] ch1 = s1.toCharArray();
-		char[] ch2 = s2.toCharArray();
+		if ((s1 != null && s2 != null) && (s1.length() == s2.length())) {
 
-		sort(ch1);
-		sort(ch2);
+			char[] ch1 = s1.toCharArray();
+			char[] ch2 = s2.toCharArray();
 
-		for (int i = 0; i < ch1.length; i++) {
-			if (ch1[i] != ch2[i]) {
-				return false;
+			sort(ch1);
+			sort(ch2);
+
+			for (int i = 0, j = 0; i < ch1.length; i++, j++) {
+				if (ch1[i] != ch2[j]) {
+					return false;
+				}
 			}
+			return true;
 		}
-		return true;
+		return false;
 	}
 
 	public static void main(String[] args) {
@@ -42,6 +40,16 @@ public class Anagram {
 			System.out.println(str1 + " and " + str2 + " are anagram");
 		} else {
 			System.out.println(str1 + " and " + str2 + " are not anagram");
+		}
+
+		System.out.println("================================================");
+
+		String str3 = "lasten";
+		String str4 = "silent";
+		if (isAnagram(str3, str4)) {
+			System.out.println(str3 + " and " + str4 + " are anagram");
+		} else {
+			System.out.println(str3 + " and " + str4 + " are not anagram");
 		}
 	}
 
