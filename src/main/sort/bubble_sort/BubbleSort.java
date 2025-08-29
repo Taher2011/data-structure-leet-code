8bd19cffe6470c31256c6228b6c71ba23c055073
Taher <taherc.ali@gmail.com>
2025-08-29 10:12:52 +0530

    buuble sort

--- src/main/sort/bubble_sort/BubbleSort.java
+++ src/main/sort/bubble_sort/BubbleSort.java
@@ -6,12 +6,18 @@ public class BubbleSort {
 
 	public static void bubbleSort(int[] nums) {
 		for (int i = nums.length - 1; i > 0; i--) {
+			boolean noSwaps = true;
 			for (int j = 0; j < i; j++) {
 				if (nums[j] > nums[j + 1]) {
 					int temp = nums[j + 1];
 					nums[j + 1] = nums[j];
 					nums[j] = temp;
+					noSwaps = false;
 				}
+			}
+			if (noSwaps) {
+				System.out.println("array is already sorted");
+				break;
 			}
 		}
 	}
