4c774b69b55485e29ed4f6a5a3713063b9cd2f78
Taher <taherc.ali@gmail.com>
2025-08-30 19:32:29 +0530

    binary search tree

--- src/main/tree/binary_tree/BinarySearchTree.java
+++ src/main/tree/binary_tree/BinarySearchTree.java
@@ -1,5 +1,8 @@
 package binary_tree;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BinarySearchTree {
 
 	private Node root;
@@ -19,49 +22,70 @@ public class BinarySearchTree {
 			root = new Node(value);
 			return true;
 		}
-		Node temp = root;
+		Node node = root;
 		while (true) {
-			if (value < temp.value) {
-				if (temp.left == null) {
-					temp.left = new Node(value);
-					return true;
-				}
-				temp = temp.left;
-			} else if (value > temp.value) {
-				if (temp.right == null) {
-					temp.right = new Node(value);
-					return true;
-				}
-				temp = temp.right;
-			} else {
+			if (value == node.value) {
 				return false;
+			}
+			if (value < node.value) {
+				if (node.left == null) {
+					node.left = new Node(value);
+					return true;
+				}
+				node = node.left;
+			} else {
+				if (value > node.value) {
+					if (node.right == null) {
+						node.right = new Node(value);
+						return true;
+					}
+					node = node.right;
+				}
 			}
 		}
 	}
 
 	public boolean contains(int value) {
-		if (root != null) {
-			if (root.value == value) {
+		if (root == null) {
+			return false;
+		}
+		Node node = root;
+		while (true) {
+			if (value == node.value) {
 				return true;
 			}
-			Node temp = root;
-			while (true) {
-				if (value < temp.value) {
-					if (temp.left == null) {
+			if (value < node.value) {
+				if (node.left == null) {
+					return false;
+				}
+				node = node.left;
+			} else {
+				if (value > node.value) {
+					if (node.right == null) {
 						return false;
 					}
-					temp = temp.left;
-				} else if (value > temp.value) {
-					if (temp.right == null) {
-						return false;
-					}
-					temp = temp.right;
-				} else {
-					return true;
+					node = node.right;
 				}
 			}
 		}
-		return false;
+	}
+
+	public List<Integer> DFSInOrder() {
+		List<Integer> list = new ArrayList<>();
+		class Traverse {
+			public Traverse(Node node) {
+				Node temp = node;
+				if (temp.left != null) {
+					new Traverse(temp.left);
+				}
+				list.add(temp.value);
+				if (temp.right != null) {
+					new Traverse(temp.right);
+				}
+			}
+		}
+		new Traverse(root);
+		return list;
 	}
 
 	public static void main(String[] args) {
@@ -75,6 +99,8 @@ public class BinarySearchTree {
 		System.out.println(bst.insert(25));
 		System.out.println(bst.insert(25));
 		System.out.println(bst.insert(28));
+		System.out.println("===========================================");
+		System.out.println(bst.DFSInOrder());
 		System.out.println("===========================================");
 		System.out.println(bst.contains(55));
 		System.out.println(bst.contains(27));
