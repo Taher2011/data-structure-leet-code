7a3729cb50be40e6a50568f0e672af95e2833917
Taher <taherc.ali@gmail.com>
2026-01-13 11:13:46 +0530

    string compression

--- src/main/java/interview/array_interview_1/string_compression/StringCompression.java
+++ src/main/java/interview/array_interview_1/string_compression/StringCompression.java
@@ -2,30 +2,27 @@ package interview.array_interview_1.string_compression;
 
 public class StringCompression {
 
-	// input --> aabcccccaaa
-	// output --> a2b1c5a3
-
-	public static String compress(String s) {
-		StringBuilder sb = new StringBuilder();
-		char[] ch = s.toCharArray();
+	public static String stringCompression(String str) {
+		StringBuilder builder = new StringBuilder();
 		int counter = 1;
-		for (int i = 1; i < ch.length; i++) {
-			if (ch[i - 1] == ch[i]) {
+		for (int i = 1; i <= str.length(); i++) {
+			if (i == str.length()) {
+				builder.append(str.charAt(i - 1)).append(counter);
+				break;
+			}
+			if (str.charAt(i - 1) == str.charAt(i)) {
 				counter++;
 			} else {
-				sb.append(ch[i - 1]).append(counter);
+				builder.append(str.charAt(i - 1)).append(counter);
 				counter = 1;
 			}
-			if (i == ch.length - 1) {
-				sb.append(ch[i - 1]).append(counter);
-			}
 		}
-		return sb.toString();
+		return builder.toString();
 	}
 
 	public static void main(String[] args) {
 		String s = "aabcccccaaaeeee";
-		System.out.println(compress(s));
+		System.out.println(stringCompression(s));
 	}
 
 }
