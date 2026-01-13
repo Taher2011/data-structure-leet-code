ac4ff2cf4bac9a5bc7a9cb8b8a52d24fd1efe192
Taher <taherc.ali@gmail.com>
2026-01-13 17:08:35 +0530

    palindrome

--- src/main/java/interview/array_interview_2/palindrome/Palindrome.java
+++ src/main/java/interview/array_interview_2/palindrome/Palindrome.java
@@ -1,5 +1,8 @@
 package interview.array_interview_2.palindrome;
 
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class Palindrome {
 
 	public static boolean isPalindrome(String str) {
@@ -10,6 +13,18 @@ public class Palindrome {
 			}
 		}
 		return true;
+	}
+
+	public static void isPalindrome(List<String> strings) {
+		for (String string : strings) {
+			IntStream index = IntStream.range(0, string.length() / 2);
+			int length = string.length() - 1;
+			if (index.allMatch(i -> string.charAt(i) == string.charAt(length - i))) {
+				System.out.println(string + " is palindrome");
+			} else {
+				System.out.println(string + " is not palindrome");
+			}
+		}
 	}
 
 	public static void main(String[] args) {
